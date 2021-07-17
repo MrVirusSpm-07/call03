289937f41657a23b40cec0c5834b5886489c0749
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-07-17 23:02:19 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+# code warna
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+# bersih teks
+clear
+# https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+xdg-open https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+# https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+# tampilan
+echo $green
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+figlet Mr Virus
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+echo $cyan"+"$red"============================================="$cyan"+"
+echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+echo $cyan"+"$red"============================================="$cyan"+"
+echo
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+echo $green"     Jangan Lupa Subrek Cenel Author"
+echo $red"==============================================="
+echo $green"{"$red"1"$green"}"$cyan" Spam Call"
+echo $green"{"$red"2"$green"}"$cyan" Join To Grup"
+echo $green"{"$red"3"$green"}"$cyan" Keluar"
+echo $red"==============================================="
+echo $green
+read -p "{•} Pilih: " sikah
+
+# data pertama
+if [ $sikah = 1 ]; then
+read -p "Klik Enter {}" no
+git clone https://github.com/projectsku/MrVirusSpm-projects
+read -p "Klik Enter {}" no
+cd MrVirusSpm-projects
+read -p "Klik Enter {}" no
+
+# tampilan
+clear
+sleep 2
+echo $green
+figlet Telpon Kuy
+echo $cyan"+"$red"============================================="$cyan"+"
+echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+echo $cyan"+"$red"============================================="$cyan"+"
+echo
+sleep 2
+echo $yellow"{"$white"+"$yellow"}"$white" Gabungan Program Antar Sheel X Python"
+echo $cyan"+"$red"============================================="$cyan"+"
+python projects03.py
+fi
+
+
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo $green"["$red"+"$green"]"$yellow" Author"$red" :"$yellow" Mr Virus Spm"
+#echo $green"["$red"+"$green"]"$yellow" Team"$red"   :"$yellow" Mafia Teknologi Indonesia"
+#echo $green"["$red"+"$green"]"$yellow" Kontak"$red" :"$yellow" +62 882-1235-2864"
+#echo $cyan"+"$red"============================================="$cyan"+"
+#echo
+
+if [ $sikah = 2 ]; then
+xdg-open https://chat.whatsapp.com/Cg93AO04QJALhwxXLD4See
+fi
+if [ $sikah = 3 ]; then
+echo "Tanks Udh Make Script Saya"
+exit
+fi
